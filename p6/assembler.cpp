cab086afff61ce149f2406df53e71b0bfef387d2
scast3 <santiagoc333@gmail.com>
2025-02-25 15:44:01 -0700

    added some symbol table text files

--- p6/assembler.cpp
+++ p6/assembler.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <fstream>
+#include <sstream>
 
-std::unordered_map<std::string, int> symbolTable;
+std::unordered_map<std::string, std::string> comp;
+std::unordered_map<std::string, std::string> dest;
+std::unordered_map<std::string, std::string> jump;
 
-int main (int argc, char** argv){
-    if (argc != 2) {
-        std::cout << "usage: assembler prog.asm" << std::endl;
+void load_map(const std::string& filename, std::unordered_map<std::string, std::string>& comp) {
+    std::ifstream infile(filename);
+    std::string line;
 
-        char* input_file = argv[1];
+    while (std::getline(infile, line)) {
+        std::istringstream iss(line);
+        std::string key, value;
 
-        first_pass(input_file);
-        
-
+        if (std::getline(iss, key, ',') && std::getline(iss, value)) {
+            comp[key] = value;
+        }
     }
+
+    infile.close();
 }
 
-void first_pass(char* inputs){
+int main (int argc, char** argv){
+
+    if (argc != 2) {
+        std::cout << "usage: assembler prog.asm" << std::endl;
+    }
+    load_map("comp.txt", comp);
+    load_map("dest.txt", dest);
+    load_map("jump.txt", jump);
+
+    int pc = -1;
+
+    char* input_file = argv[1];
+
+    first_pass(input_file, pc);
+
+}
+
+void first_pass(char* inputs, int pc){
 
 }
 
 void second_pass(){
-
+    
 }
 
 void next_command(){
